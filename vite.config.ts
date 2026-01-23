525bffb00e6bac34648bdb61d5caf628991b25bd
Dong Bich Ngoc <hanoiklab@ssem.re.kr>
2026-01-23 15:47:16 +0700

    Fix paths in index.html for production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/eduone-global-camp-2026/',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
